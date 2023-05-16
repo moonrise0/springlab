51cc21e6a4d854f4d0b761308715f2ee1ab6950f
moonrise0 <moonriseisnow@gmail.com>
2023-05-16 14:50:27 +0900

    "0516"

--- src/main/java/com/kbstar/Web39Application.java
+++ src/main/java/com/kbstar/Web39Application.java
@@ -6,10 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @SpringBootApplication
-public class Web38Application {
+public class Web39Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Web38Application.class, args);
+        SpringApplication.run(Web39Application.class, args);
     }
     @Bean
     public InternalResourceViewResolver setupViewResolver() {
